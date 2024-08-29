7f090a9cdaaabfd94f1ebf1b9169154692d50767
Umesh <umesh.udayakumar@trenser.com>
2024-08-29 17:37:42 +0530

    Completed task form validation with  disable button click.

--- Assignment-4-formWithOnchange/formScript.js
+++ Assignment-4-formWithOnchange/formScript.js
@@ -3,6 +3,7 @@ document.getElementById("age").onchange = function () { ageValidation() };
 document.getElementById("address").onchange = function () { addressValidation() };
 document.getElementById("email").onchange = function () { emailValidation() };
 document.getElementById("phone").onchange = function () { phoneValidation() };
+let submitButton = document.getElementById("btn"); let nameValid = false, passwordValid = false, ageValid = false, emailVailid = false, addressValid = false; phoneValid = false;
 
 function nameValidation(e) {
     console.log("event", e.target.value);
@@ -10,9 +11,12 @@ function nameValidation(e) {
     const nameError = document.getElementById("name-error");
     if (name === "" || !(/^[A-Za-z]+$/.test(name))) {
         nameError.textContent = "Please enter the name properly";
-    }
-    else {
+        nameValid = false;
+        validation();
+    } else {
         nameError.textContent = "";
+        nameValid = true;
+        validation();
     }
 }
 
@@ -21,9 +25,12 @@ function passwordValidation() {
     const passwordError = document.getElementById("password-error");
     if (password === "" || !(/^(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(password))) {
         passwordError.textContent = "Password must contain atleast 8 characters including uppercase and lowercase";
-    }
-    else {
+        passwordValid = false;
+        validation();
+    } else {
         passwordError.textContent = "";
+        passwordValid = true;
+        validation();
     }
 }
 
@@ -32,9 +39,12 @@ function ageValidation() {
     const ageError = document.getElementById("age-error");
     if (age === "" || !(/^(?!0$)[1-9]\d*$/.test(age)) || parseInt(age) < 18 || parseInt(age) > 100) {
         ageError.textContent = "Please enter a valid age between 18 and 100";
-    }
-    else {
+        ageValid = false;
+        validation();
+    } else {
         ageError.textContent = "";
+        ageValid = true;
+        validation();
     }
 }
 
@@ -43,9 +53,12 @@ function addressValidation() {
     const addressError = document.getElementById("address-error");
     if (address === "") {
         addressError.textContent = "Please enter the address.";
-    }
-    else {
+        addressValid = false;
+        validation();
+    } else {
         addressError.textContent = "";
+        addressValid = true;
+        validation();
     }
 }
 
@@ -54,9 +67,12 @@ function emailValidation() {
     const emailError = document.getElementById("email-error");
     if (email === "" || !(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email))) {
         emailError.textContent = "Please enter a valid email address.";
-    }
-    else {
+        emailValid = false;
+        validation();
+    } else {
         emailError.textContent = "";
+        emailValid = true;
+        validation();
     }
 }
 
@@ -65,8 +81,19 @@ function phoneValidation() {
     const phoneError = document.getElementById("phone-error");
     if (phone === "" || !(/^\d{10}$/.test(phone))) {
         phoneError.textContent = "Please enter a valid phone number.";
-    }
-    else {
+        phoneValid = false;
+        validation();
+    } else {
         phoneError.textContent = "";
+        phoneValid = true;
+        validation();
+    }
+}
+
+function validation() {
+    if (nameValid && passwordValid && ageValid && addressValid && emailValid && phoneValid) {
+        submitButton.disabled = false;
+    } else {
+        submitButton.disabled = true;
     }
 }
