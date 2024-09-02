548747afeb66b558b154245c3f428d308fff765c
Umesh <umesh.udayakumar@trenser.com>
2024-09-02 17:44:35 +0530

    Added functionality for calculating more than two numbers and styled accordingly.

--- Assignment-calculator/CalcScript.js
+++ Assignment-calculator/CalcScript.js
@@ -1,67 +1,76 @@
 function clearScreen() {
-    document.getElementById("calculator-display").value = "";
+  document.getElementById("calculator-display").value = "";
 }
 
 function addToDisplay(buttonValue) {
-    document.getElementById("calculator-display").value += buttonValue;
+  document.getElementById("calculator-display").value += buttonValue;
 }
 
 function deleteLastElement() {
-    let currentValue=document.getElementById("calculator-display").value;
-    document.getElementById("calculator-display").value = currentValue.slice(0,-1);
+  let currentValue = document.getElementById("calculator-display").value;
+  document.getElementById("calculator-display").value = currentValue.slice(0,-1);
 }
 
 function calculateResult() {
-    let currentValue=document.getElementById("calculator-display").value;
-    let lengthOfInput=currentValue.length;
-    let firstValue,secondValue,operator,result;
-    let percentageFlag=false;
-    for(let i=0;i<lengthOfInput;i++) {
-        if(currentValue[i]==='+'||currentValue[i]==='-'||currentValue[i]==='x'||currentValue[i]==='/'||currentValue[i]==='%') {
-            firstValue=currentValue.slice(0,i);
-            operator=currentValue[i];
-            console.log(operator);
-            if(currentValue[i]==='%') {
-                firstValue=parseFloat(firstValue);
-                result=calculatePercentage(firstValue);
-                percentageFlag=true;
-                break;
-            }
-            secondValue=currentValue.slice(i+1,lengthOfInput);
-            break;
-        }
-    }
-    if(percentageFlag) {
-        document.getElementById("calculator-display").value =result;
-    } else {
-        firstValue=parseFloat(firstValue);
-        secondValue=parseFloat(secondValue);
-        result=calculateOperation(firstValue,secondValue,operator);
-        document.getElementById("calculator-display").value =result;
-    }
+  let currentValue = document.getElementById("calculator-display").value;
+  currentValue = processPercentage(currentValue);
+  let numbers = currentValue.split(/[\+\-\x\/]/).map(Number);
+  let operators = currentValue.split(/[\d.]+/).filter(Boolean);
+  result = processOperations(numbers, operators);
+  document.getElementById("calculator-display").value = result;
 }
 
-function calculateOperation(firstValue,secondValue,operator) {
-    switch (operator) {
-        case '+':
-            return firstValue+secondValue;
-            break;
-        case '-':
-            return firstValue-secondValue;
-            break;
-        case 'x':
-            return firstValue*secondValue;
-            break;
-        case '/':
-            return firstValue/secondValue;
-            break;
-        default:
-            break;
+function processOperations(numbers, operators) {
+  for (let i = 0; i < operators.length; i++) {
+    if (operators[i] === "x" || operators[i] === "/") {
+      let result = calculateOperation(numbers[i], numbers[i + 1], operators[i]);
+      numbers.splice(i, 2, result);
+      operators.splice(i, 1);
+      i--;
     }
+  }
+  for (let i = 0; i < operators.length; i++) {
+    let result = calculateOperation(numbers[i], numbers[i + 1], operators[i]);
+    numbers.splice(i, 2, result);
+    operators.splice(i, 1);
+    i--;
+  }
+  return numbers[0];
+}
+
+function calculateOperation(firstValue, secondValue, operator) {
+  switch (operator) {
+    case "+":
+      return firstValue + secondValue;
+      break;
+    case "-":
+      return firstValue - secondValue;
+      break;
+    case "x":
+      return firstValue * secondValue;
+      break;
+    case "/":
+      return firstValue / secondValue;
+      break;
+    default:
+      break;
+  }
+}
+
+function processPercentage(expression) {
+  let percentageRegex = /\d+%/g;
+  let isMatch = expression.match(percentageRegex);
+  if (isMatch) {
+    isMatch.forEach((match) => {
+      let number = parseFloat(match);
+      let percentageValue = calculatePercentage(number);
+      expression = expression.replace(match, percentageValue);
+    });
+  }
+  return expression;
 }
 
 function calculatePercentage(firstValue) {
-    console.log(firstValue);
-    const result=firstValue/100;
-    return result;
+  const result = firstValue / 100;
+  return result;
 }
